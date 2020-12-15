afd49631253e4531f8ab704042ba882126d99231
Julien Bordas <julien.bordas@amadeus.com>
2020-12-15 16:38:33 +0100

    Add'em Up

---
+++ Puzzles/Easy/AddemUp.py
@@ -0,0 +1,13 @@
+import sys
+import math
+
+n = int(input())
+x = sorted(map(int, input().split()))
+
+cost = 0
+while len(x) > 1:
+    minSum = x[0] + x[1]
+    cost += minSum
+    x = sorted([minSum] + x[2:])
+
+print(cost)
