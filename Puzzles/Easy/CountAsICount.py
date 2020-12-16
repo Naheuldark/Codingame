ecd06bd896aa41af82ef2c4a42abe6f5d73807cb
Julien Bordas <julien.bordas@amadeus.com>
2020-12-16 16:11:54 +0100

    Count as I count

---
+++ Puzzles/Easy/CountAsICount.py
@@ -0,0 +1,11 @@
+import sys
+import math
+
+initial_score = int(input())
+combs = [[1, 0, 0, 0, 0]] + [[0, 0, 0, 0, 0] for _ in range(50 - initial_score)]
+
+for i in range(1, len(combs)):
+    for j in range(max(0, i - 12), i):
+        for k in range(1, 5):
+            combs[i][k] += combs[j][k - 1] * (2 - (j == i - 1))
+print(sum(combs[i][1:]))
