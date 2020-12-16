edc9ac23ea9c152af138156aedad6a9f6d5ecb4c
Julien Bordas <julien.bordas@amadeus.com>
2020-12-16 16:55:43 +0100

    Smooth!

---
+++ Puzzles/Easy/Smooth.py
@@ -0,0 +1,17 @@
+import sys
+import math
+
+for _ in range(int(input())):
+    f = int(input())
+    while f > 1:
+        if f % 5 == 0:
+            f //= 5
+        elif f % 3 == 0:
+            f //= 3
+        elif f % 2 == 0:
+            f //= 2
+        else:
+            print('DEFEAT')
+            break
+    else:
+        print('VICTORY')
